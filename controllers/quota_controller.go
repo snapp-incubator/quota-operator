3809ce5f0a032fd1fdb505b4353a879e8cffacf0
Mohammad Yosefpor <myusefpur@gmail.com>
2021-07-31 01:52:25 +0430

    fix permission

--- controllers/quota_controller.go
+++ controllers/quota_controller.go
@@ -41,6 +41,7 @@ type QuotaReconciler struct {
 //+kubebuilder:rbac:groups=quota.snappcloud.io,resources=quotas,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=quota.snappcloud.io,resources=quotas/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=quota.snappcloud.io,resources=quotas/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=resourcequota,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
