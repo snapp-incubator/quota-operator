88182d23f1d1152266599ca35dd522685fe3d209
SaDeGh <stabasi76@gmail.com>
2022-06-19 17:00:23 +0430

    some change

--- main.go
+++ main.go
@@ -19,19 +19,15 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
-	"net/http"
 	"os"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/api/rbac/v1"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	openshiftquotav1 "github.com/openshift/api/quota/v1"
 	quotav1alpha1 "github.com/snapp-cab/quota-operator/api/v1alpha1"
-	"github.com/snapp-cab/quota-operator/controllers"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
@@ -67,54 +63,31 @@ const (
 )
 
 func (v *resourceQuotaValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
-
 	if req.Operation == "UPDATE" {
-
 		ns := &corev1.Namespace{}
 		err := v.Client.Get(context.TODO(), types.NamespacedName{Name: req.Namespace}, ns)
 		if err != nil {
 			setupLog.Error(err, "error getting namespace", "name", req.Namespace)
 			return admission.Denied("error on getting namespace")
 		}
-
 		l, ok := ns.GetLabels()[teamLabel]
 		if !ok {
 			return admission.Denied("no team found for the project. please join your project to a team")
 		}
-
 		crq := &openshiftquotav1.ClusterResourceQuota{}
 		err = v.Client.Get(context.TODO(), types.NamespacedName{Name: l}, crq)
 		if err != nil {
 			setupLog.Error(err, "error getting clusterResourceQuota", "name", l)
 			return admission.Denied("no team quota found. please request a quota for your team in cloud-support")
 		}
-
-		roleBinding := &v1.RoleBinding{}
-		err = v.Client.Get(context.TODO(), types.NamespacedName{Name: "admin", Namespace: req.Namespace}, roleBinding)
-		if err != nil {
-			setupLog.Error(err, "unable to get admin rolebinding")
-			return admission.Denied("Error on getting rolebinding list")
-		} else {
-			setupLog.Info("rolebinding", "rolebinding", roleBinding)
-		}
-
-		roleBindingSubjects := roleBinding.Subjects
-		for _, subject := range roleBindingSubjects {
-			setupLog.Info("subject", "subject", subject.Name)
-			if req.UserInfo.Username == subject.Name {
-				setupLog.Info("user is admin", "user", req.UserInfo.Username)
-				return admission.Allowed("user is admin")
-			}
-		}
-
-		return admission.Denied("Akharin Denied")
-	} else if req.Operation == "CREATE" {
-		// maybe need some validating rules for creating??
-		return admission.Allowed("CREATE")
+		return admission.Allowed("updating resourcequota")
 	} else if req.Operation == "DELETE" {
-		return admission.Denied("FELAN DELETE NADARIM :) ")
+		if req.Name == "default" {
+			return admission.Denied("default resourcequota cannot be deleted")
+		}
+		return admission.Allowed("DELETE")
 	} else {
-		return admission.Denied("Akharinnnnnn")
+		return admission.Allowed("Allowed")
 	}
 }
 
