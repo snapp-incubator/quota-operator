8d568dd32dc0bc437ca8ad00e397edbff40e80ce
SaDeGh <stabasi76@gmail.com>
2022-06-20 21:58:22 +0430

    delete rbac for accessing rbac

--- custom_webhooks/resourcequota_webhook.go
+++ custom_webhooks/resourcequota_webhook.go
@@ -12,7 +12,6 @@ import (
 )
 
 //+kubebuilder:webhook:path=/validate-v1-resource-quota,mutating=false,failurePolicy=fail,sideEffects=None,groups="",resources=resourcequotas,verbs=create;update;delete,versions=v1,name=vresourcequota.kb.io,admissionReviewVersions={v1,v1beta1}
-//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=resourcequotas,verbs=get;watch;list
 
 type ResourceQuotaValidator struct {
 	Client client.Client
