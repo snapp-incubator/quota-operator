a42fdb6699d2ebd88e4f714ff2545f8d63b48c9d
Snapp <snapp@Snapps-MacBook-Pro.local>
2022-06-28 11:25:30 -0700

    fix bug

--- custom_webhooks/resourcequota_webhook.go
+++ custom_webhooks/resourcequota_webhook.go
@@ -1,40 +1,40 @@
 package custom_webhook
 
 import (
-    "context"
+	"context"
 
-    openshiftquotav1 "github.com/openshift/api/quota/v1"
-    corev1 "k8s.io/api/core/v1"
-    "k8s.io/apimachinery/pkg/types"
-    "sigs.k8s.io/controller-runtime/pkg/client"
-    "sigs.k8s.io/controller-runtime/pkg/log"
-    "sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+	openshiftquotav1 "github.com/openshift/api/quota/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 //+kubebuilder:webhook:path=/validate-v1-resource-quota,mutating=false,failurePolicy=fail,sideEffects=None,groups="",resources=resourcequotas,verbs=create;update;delete,versions=v1,name=vresourcequota.kb.io,admissionReviewVersions={v1,v1beta1}
 
 type ResourceQuotaValidator struct {
-    Client client.Client
+	Client client.Client
 }
 
 const (
-    teamLabel = "snappcloud.io/team"
-    enforceLabel = "quota.snappcloud.io/enforce"
+	teamLabel    = "snappcloud.io/team"
+	enforceLabel = "quota.snappcloud.io/enforce"
 )
 
 func (v *ResourceQuotaValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
-    log := log.FromContext(ctx)
-    if req.Operation == "UPDATE" {
-        ns := &corev1.Namespace{}
-        err := v.Client.Get(context.TODO(), types.NamespacedName{Name: req.Namespace}, ns)
-        if err != nil {
-            log.Error(err, "error getting namespace", "name", req.Namespace)
-            return admission.Denied("error on getting namespace")
-        }
-        if l,ok := ns.GetLabels()[enforceLabel]; ok{
-            if l == "true"{
-			 return admission.Allowed("updating resourcequota")
-            }else if l == "false" || l == ""{
+	log := log.FromContext(ctx)
+	if req.Operation == "UPDATE" {
+		ns := &corev1.Namespace{}
+		err := v.Client.Get(context.TODO(), types.NamespacedName{Name: req.Namespace}, ns)
+		if err != nil {
+			log.Error(err, "error getting namespace", "name", req.Namespace)
+			return admission.Denied("error on getting namespace")
+		}
+		if l, ok := ns.GetLabels()[enforceLabel]; ok {
+			if l == "true" {
+				return admission.Allowed("updating resourcequota")
+			} else if l == "false" || l == "" {
 				l, ok := ns.GetLabels()[teamLabel]
 				if !ok {
 					return admission.Denied("no team found for the project. please join your project to a team")
@@ -46,16 +46,16 @@ func (v *ResourceQuotaValidator) Handle(ctx context.Context, req admission.Reque
 					return admission.Denied("no team quota found. please request a quota for your team in cloud-support")
 				}
 				return admission.Allowed("updating resourcequota")
-			}else{
+			} else {
 				return admission.Denied("no enforce label found for the project")
-			}	
-        }
-    } else if req.Operation == "DELETE" {
-        if req.Name == "default" {
-            return admission.Denied("default resourcequota cannot be deleted")
-        }
-        return admission.Allowed("DELETE")
-    } else {
-        return admission.Allowed("Allowed")
-    }
+			}
+		}
+	} else if req.Operation == "DELETE" {
+		if req.Name == "default" {
+			return admission.Denied("default resourcequota cannot be deleted")
+		}
+		return admission.Allowed("DELETE")
+	} else {
+		return admission.Allowed("Allowed")
+	}
 }
